9826fbe7f4f1c2531cc8d0a88ce7faedf218ba76
Gaurav <parihargaurav527@gmail.com>
2023-10-16 17:55:32 +0530

    first commit

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
